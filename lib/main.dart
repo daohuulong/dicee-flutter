654219b1d6d9a9b51c14e8a8ac7ca68481804815
daohuulong <daohuulong@gmail.com>
2020-07-08 16:44:53 +0700

    +stateless

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,26 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('left button pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('left button pressed');
+                },
+                child: Image.asset('images/dice2.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
